d34b6a1f370add4cbe9dd3603dcae0b867e32078
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2023-11-25 11:20:20 -0800

    automatically crop large scans

--- extract_points.py
+++ extract_points.py
@@ -8,13 +8,51 @@ from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu, gaussian
 from skimage.segmentation import flood
 
-
 import matplotlib.pyplot as plt
 
 
 def contained(inner,outer):
     return outer[0] < inner[0] and outer[1] < inner[1] and inner[2] < outer[2] and inner[3] < outer[3]
 
+"""
+def component_bounding_boxes(labels, min_size = 100):
+    rp = measure.regionprops(labels)
+    good_regions = []
+    for p in measure.regionprops(labels):
+        xmin,ymin,xmax,ymax = p.bbox
+        if  (xmax - xmin) < min_size or (ymax - ymin) < min_size:
+            continue
+        good_regions.append(p)
+ """
+
+def identify_crop(binary_image, raw_image, min_size = 100): 
+    labels = measure.label(morphology.binary_dilation(binary_image), background = True)
+    
+    good_regions = []
+    for p in measure.regionprops(labels):
+        xmin,ymin,xmax,ymax = p.bbox
+        if  (xmax - xmin) < min_size or (ymax - ymin) < min_size:
+            continue
+        good_regions.append(p)
+              
+    if len(good_regions) == 0:
+        print("Unable to find any crop region - check thresholding parameters and minimum size and try again")
+        return None
+    elif len(good_regions) > 1:
+        print("Found too many crop regions - check thresholding parameters, decrease minimum size, or manually crop image.")
+        return None
+
+    # Now we mask out the raw image
+    p = good_regions[0]
+    xmin,ymin,xmax,ymax = p.bbox
+
+    m = 5
+    mask = morphology.binary_erosion(p.image_filled, footprint=[(np.ones((m, 1)), 1), (np.ones((1, m)), 1)])
+    crop = raw_image[xmin:xmax, ymin:ymax]
+
+    crop[np.logical_not(mask)] = sum(sum(crop * mask)) / sum(sum(mask))
+    return np.array([xmin, ymin]), crop
+    
 def identify_roi(binary_image, n):
     
     labels = measure.label(morphology.binary_erosion(binary_image))
@@ -70,6 +108,7 @@ def find_mark_location(image, binary, box):
 
 @click.command()
 @click.option('--negative/--positive', default = True, help = "Default case (--negative) is light marks on a dark background. --positive inverts this.")
+@click.option('--crop/--no-crop', default = True, help = "Attempt to crop a full scan to the boundaries of a test coupon")
 @click.option('--threshold', default = 2.5, type = click.FloatRange(0.0, 20), help = "User-adjustable parameter for image binarization - larger values bias towards black.")
 @click.option('--points', default = 11, type = int, help = "Number of points on the side of the test pattern")
 @click.option('--plot/--silent', default = False, help = "Display the segmented image and final result, or just output points.")
@@ -84,6 +123,16 @@ def cal(*args, **kwargs):
     thresh = kwargs['threshold'] * threshold_otsu(smoothed)
     binary = (smoothed > thresh)
 
+    if kwargs['crop']:
+        crop = identify_crop(binary, image)
+        if crop is None:
+            exit()
+        _, image = crop
+        smoothed = gaussian(image, sigma=2)
+        thresh = kwargs['threshold'] * threshold_otsu(smoothed)
+        binary = (smoothed > thresh)
+            
+    
     if not kwargs['negative']:
         binary = np.logical_not(binary)
         
